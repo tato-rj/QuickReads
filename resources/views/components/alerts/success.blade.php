4eedc331bedf063928b2910054fb0485e10eacf9
Arthur Villar <=>
2018-01-18 23:59:22 -0500

    Update alert box

--- resources/views/components/alerts/success.blade.php
+++ resources/views/components/alerts/success.blade.php
@@ -1,6 +1,7 @@
 @component('components/alerts/alert')
 	@slot('alert')success
 	@endslot
-	@slot('message'){{$message}}
+	@slot('message')
+		<strong class="mr-2">Success |  </strong>{{$message}}
 	@endslot
 @endcomponent
